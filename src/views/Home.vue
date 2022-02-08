4bf7c9b661e5d4015ec6a488cff1404854cea618
draganet <3417237276@qq.com>
2022-02-08 17:14:30 +0800

    搭建 html 结构

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="topnar">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>wheel UI</h1>
+      <h2>一个厉害的UI</h2>
+      <p class="actions">
+        <a href="">github</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.topnar {
+  border: 1px solid red;
+}
+</style>
