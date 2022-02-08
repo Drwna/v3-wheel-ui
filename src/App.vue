254324a5d1758565f4aa45021d410d6a836058a2
draganet <3417237276@qq.com>
2022-02-08 21:38:03 +0800

    路由切换后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,20 @@
 
 <script lang="ts">
 import { provide, ref } from "vue";
+import { router } from "./router";
+
 export default {
   name: "App",
   setup() {
     const width = document.documentElement.clientWidth;
     const asideVisible = ref(width <= 500 ? false : true); // set
     provide("asideVisible", asideVisible);
+
+    router.afterEach(() => {
+      if (width <= 500) {
+        asideVisible.value = false;
+      }
+    });
   },
 };
 </script>
