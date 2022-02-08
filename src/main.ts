34aa32c3a54fc201d0b87d9f0d5ec78a3216a2ac
draganet <3417237276@qq.com>
2022-02-08 17:27:14 +0800

    topnav 样式更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import App from "./App.vue";
 import { createApp } from "vue";
-import "./index.css";
+import "./index.scss";
 import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./views/Home.vue";
 import Doc from "./views/Doc.vue";
