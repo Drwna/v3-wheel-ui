f66d090eebd24bcaa5300217f008fcd6f9490bca
draganet <3417237276@qq.com>
2022-02-09 20:24:48 +0800

    dialog 实现点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,16 @@
 <template>
   <template v-if="visible">
-    <div class="wheel-dialog-overlay"></div>
+    <div class="wheel-dialog-overlay" @click="onClickOverlay"></div>
     <div class="wheel-dialog-wrapper">
       <div class="wheel-dialog">
-        <header>标题 <span class="wheel-dialog-close"></span></header>
+        <header>标题 <span @click="close" class="wheel-dialog-close"></span></header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -25,9 +25,41 @@ export default {
     visible: {
       type: Boolean,
       default: false
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function
     }
   },
   components: {Button},
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        // props.ok && props.ok !== false
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit('cancel');
+      close();
+    };
+
+    return {close, onClickOverlay, ok, cancel};
+  }
 };
 </script>
 
