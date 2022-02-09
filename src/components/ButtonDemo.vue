eb9aae2d017d98adce61e8a5d5d0c7eda0f215d7
draganet <3417237276@qq.com>
2022-02-09 11:58:56 +0800

    button 属性绑定示例

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick" @mouseover="onClick" size="small">你好</Button>
   </div>
 </template>
 
@@ -11,5 +11,11 @@ import Button from "../lib/Button.vue";
 
 export default {
   components: { Button },
+  setup() {
+    const onClick = () => {
+      alert("hello");
+    };
+    return { onClick };
+  },
 };
 </script>
