8b9fadad762597c885285545647ff9d1f2fa11c0
draganet <3417237276@qq.com>
2022-02-09 16:08:25 +0800

    button size 示例样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,18 +1,29 @@
 <template>
   <div>Button 示例</div>
   <h1>示例1</h1>
-  <div>
+  <div class="example">
     <Button>hello</Button>
-    <Button theme="button">你好</Button>
-    <Button theme="link">你好</Button>
-    <Button theme="text">你好</Button>
+    <Button theme="button">hello</Button>
+    <Button theme="link">hello</Button>
+    <Button theme="text">hello</Button>
   </div>
   <h1>示例2</h1>
   <div>
-    <Button>默认</Button>
-    <Button size="small">小按钮</Button>
-    <Button size="normal">普通大小</Button>
-    <Button size="big">大按钮</Button>
+    <div class="example">
+      <Button size="big">大大大按钮</Button>
+      <Button>默认按钮</Button>
+      <Button size="small">小小小按钮</Button>
+    </div>
+    <div class="example">
+      <Button theme="link" size="big">大大大按钮</Button>
+      <Button theme="link">默认按钮</Button>
+      <Button theme="link" size="small">小小小按钮</Button>
+    </div>
+    <div class="example">
+      <Button size="big" theme="text">大大大按钮</Button>
+      <Button theme="text">默认按钮</Button>
+      <Button theme="text" size="small">小小小按钮</Button>
+    </div>
   </div>
 </template>
 
@@ -29,3 +40,11 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.example {
+  height: 90px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+</style>
