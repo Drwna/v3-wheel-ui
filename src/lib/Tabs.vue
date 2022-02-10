e1ea9789f925935ecfcadb76279dd55250f2fef5
draganet <3417237276@qq.com>
2022-02-10 11:38:01 +0800

    Tabs 导航 css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <div v-for="(t,index) in titles" :key="index">
-      {{ t }}
+    <div class="wheel-tabs">
+      <div class="wheel-tabs-nav">
+        <div class="wheel-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+      </div>
+      <div class="wheel-tabs-content">
+        <component class="wheel-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      </div>
     </div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
   </div>
 </template>
 
@@ -24,6 +28,30 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
 
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.wheel-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
