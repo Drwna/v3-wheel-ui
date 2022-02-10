7495c85fe5f9c1eae6873032149d3a54619441ad
draganet <3417237276@qq.com>
2022-02-10 22:59:45 +0800

    文档页 高亮当前路由

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -52,6 +52,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -83,7 +84,6 @@ export default {
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
   position: fixed;
   top: 0;
   left: 0;
@@ -91,10 +91,18 @@ aside {
   height: 100%;
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
   > ol {
     > li {
-      padding: 4px 0;
+      > a {
+        display: block;
+        padding: 4px 16px;
+      }
+      .router-link-active {
+        color: red;
+        background: white;
+      }
     }
   }
 }
