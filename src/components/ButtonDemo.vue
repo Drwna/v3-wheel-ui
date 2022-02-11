8f927fcf79b25b82065d5bfae758d85ec7655ab0
draganet <3417237276@qq.com>
2022-02-11 17:57:17 +0800

    ButtonDemo

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,78 +1,37 @@
 <template>
-  <h1>示例1</h1>
-  <div class="example">
-    <Button>hello</Button>
-    <Button theme="button">hello</Button>
-    <Button theme="link">hello</Button>
-    <Button theme="text">hello</Button>
-  </div>
-  <h1>示例2</h1>
-  <div>
-    <div class="example">
-      <Button size="big">大按钮</Button>
-      <Button>默认按钮</Button>
-      <Button size="small">小按钮</Button>
-    </div>
-    <div class="example">
-      <Button theme="link" size="big">大按钮</Button>
-      <Button theme="link">默认按钮</Button>
-      <Button theme="link" size="small">小按钮</Button>
-    </div>
-    <div class="example">
-      <Button size="big" theme="text">大按钮</Button>
-      <Button theme="text">默认按钮</Button>
-      <Button theme="text" size="small">小按钮</Button>
-    </div>
-  </div>
-  <h1>示例3</h1>
-  <div>
-    <div class="example">
-      <Button level="main">主要按钮</Button>
-      <Button>普通按钮</Button>
-      <Button level="danger">危险按钮</Button>
-    </div>
-    <div class="example">
-      <Button theme="link" level="main">主要链接按钮</Button>
-      <Button theme="link">普通链接按钮</Button>
-      <Button theme="link" level="danger">危险链接按钮</Button>
-    </div>
-    <div class="example">
-      <Button theme="text" level="main">主要文字按钮</Button>
-      <Button theme="text">普通文字按钮</Button>
-      <Button theme="text" level="danger">危险文字按钮</Button>
-    </div>
-    <h1>示例4</h1>
-    <div class="example">
-      <Button disabled>禁用按钮</Button>
-      <Button theme="link" disabled>禁用链接按钮</Button>
-      <Button theme="text" disabled>禁用按钮</Button>
-    </div>
-    <div>示例5</div>
-    <div class="example">
-      <Button loading>加载中</Button>
-      <Button>加载完毕</Button>
-    </div>
-  </div>
+  <Demo :component="Button1Demo"/>
+  <Demo :component="Button2Demo"/>
+  <Demo :component="Button3Demo"/>
+  <Demo :component="Button4Demo"/>
+  <Demo :component="Button5Demo"/>
 </template>
 
 <script lang="ts">
-import Button from "../lib/Button.vue";
+import Demo from './Demo.vue';
+import Button1Demo from './Button1.demo.vue';
+import Button2Demo from './Button2.demo.vue';
+import Button3Demo from './Button3.demo.vue';
+import Button4Demo from './Button4.demo.vue';
+import Button5Demo from './Button5.demo.vue';
 
 export default {
-  components: { Button },
+  components: {
+    Demo,
+    Button1Demo,
+    Button2Demo,
+    Button3Demo,
+    Button4Demo,
+    Button5Demo
+  },
   setup() {
-    const onClick = () => {
-      alert("hello");
+    return {
+      Demo,
+      Button1Demo,
+      Button2Demo,
+      Button3Demo,
+      Button4Demo,
+      Button5Demo
     };
-    return { onClick };
   },
 };
 </script>
-<style lang="scss" scoped>
-.example {
-  height: 80px;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-}
-</style>
